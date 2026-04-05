4f07c8ee9a08c103b1f9a55415e4d99a59383ae2
jinhui <jinhuiz0108@163.com>
2026-04-05 17:07:54 +0800

    refactor: simplify notes to display-only, remove search and create/edit

--- src/api/note.ts
+++ src/api/note.ts
@@ -12,20 +12,6 @@ export interface FileContent {
   content: string;
 }
 
-export interface SaveResponse {
-  path: string;
-  content: string;
-  git_message?: string;
-}
-
-export interface SearchResult {
-  path: string;
-  name: string;
-  type: string;
-  matched_in: string;
-  snippet?: string;
-}
-
 export const noteApi = {
   getTree: (path: string = '') => {
     return axios.get<FileNode[]>('/notes/tree', { params: { path } });
@@ -33,25 +19,5 @@ export const noteApi = {
 
   getFile: (path: string) => {
     return axios.get<FileContent>('/notes/file', { params: { path } });
-  },
-
-  search: (keyword: string) => {
-    return axios.get<SearchResult[]>('/notes/search', { params: { keyword } });
-  },
-
-  createFile: (path: string, content: string = '') => {
-    return axios.post<SaveResponse>('/notes/file', { path, content });
-  },
-
-  updateFile: (path: string, content: string) => {
-    return axios.put<SaveResponse>('/notes/file', { path, content });
-  },
-
-  deleteFile: (path: string) => {
-    return axios.delete('/notes/file', { params: { path } });
-  },
-
-  createDirectory: (path: string) => {
-    return axios.post('/notes/directory', null, { params: { path } });
   },
 };
